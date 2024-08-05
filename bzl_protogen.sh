2fc58f87c9c76cb322d5c92429e4ac658d53180c
Oleg Dashevskii <odashevskii@plaid.com>
2024-08-05 14:56:22 +0700

    wip

--- bzl_protogen.sh
+++ bzl_protogen.sh
@@ -3,38 +3,46 @@
 set -euo pipefail
 
 usage() {
-	echo "Usage: $0 [-v] [all|incremental]"
-	echo "  -v          : be verbose"
-	echo "  all         : Run diff tests and regen what fails"
-	echo "  incremental : Use rdeps"
+	echo "Usage: $0 [-v] [-b MERGE_BASE] [all|incremental]"
+	echo "  -v            : be verbose"
+	echo "  -b MERGE_BASE : defines merge base for incremental mode (default: origin/main)"
+	echo "  all           : Run diff tests and regen what fails (default mode)"
+	echo "  incremental   : Use rdeps"
 	exit 1
 }
 
 VERBOSE=""
+MERGE_BASE="origin/main"
 
 all() {
 	test_list_file=$(mktemp)
 	test_errors_file=$(mktemp)
  	bazel query 'kind(diff_test, //lib/proto/...)' 2>/dev/null > "$test_list_file"
 
+	[[ -n "$VERBOSE" ]] && echo "$(cat "$test_list_file" | wc -l | perl  -pe 's/^\s+//g') diff test(s) to be run"
+
 	if ! bazel test --test_summary none --test_output errors --target_pattern_file "$test_list_file" >"$test_errors_file" 2>/dev/null; then
 		while IFS= read -r line; do
-			if [[ ! -z "$VERBOSE" ]]; then
-				echo "$line"
-			fi
+			[[ -n "$VERBOSE" ]] && echo "$line"
 			eval "$line"
 		done <<< "$(cat "$test_errors_file" | grep 'bazel run //lib/proto/' | perl -pe 's/^\s+//g' | sort | uniq)"
+	else
+		[[ -n "$VERBOSE" ]] && echo "No relevant proto source updates"
 	fi
 }
 
 incremental() {
-	echo inc
+	echo "Not implemented yet"
+	exit 1
 }
 
-while getopts "v" opt; do
+while getopts "vb:" opt; do
 	case ${opt} in
 		v)
 			VERBOSE=1
+			;;
+		b)
+			MERGE_BASE=${OPTARG}
 			;;
         *)
             usage
@@ -44,9 +52,7 @@ done
 
 shift $((OPTIND -1))
 
-cmd="${1:-incremental}"
-
-case "$cmd" in
+case "${1:-all}" in
 	all)
 		all
 		;;
